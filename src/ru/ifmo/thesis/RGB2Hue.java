756813cd42e31f7e0aea42c87009d158144982f4
knovakovskaya <k9119097207@gmail.com>
2015-02-18 15:01:41 +0300

    Initial. R.Shevchenko version of program
    code moved to convention-named packages, but didnt changed.

---
+++ src/ru/ifmo/thesis/RGB2Hue.java
@@ -0,0 +1,27 @@
+package ru.ifmo.thesis;
+
+
+public abstract class RGB2Hue {
+
+   public static int ApplyFilter(int red, int green, int blue) {
+      int HSV_H = 0;
+
+      double MaxHSV = (Math.max(red, Math.max(green, blue)));
+      double MinHSV = (Math.min(red, Math.min(green, blue)));
+
+      if (MaxHSV != MinHSV) {
+         int IntegerMaxHSV = (int) (MaxHSV);
+         if (IntegerMaxHSV == red && green >= blue) {
+            HSV_H = (int) (60 * (green - blue) / (MaxHSV - MinHSV));
+         } else if (IntegerMaxHSV == red && green < blue) {
+            HSV_H = (int) (359 + 60 * (green - blue) / (MaxHSV - MinHSV));
+         } else if (IntegerMaxHSV == green) {
+            HSV_H = (int) (119 + 60 * (blue - red) / (MaxHSV - MinHSV));
+         } else if (IntegerMaxHSV == blue) {
+            HSV_H = (int) (239 + 60 * (red - green) / (MaxHSV - MinHSV));
+         }
+      } else HSV_H = 0;
+
+      return HSV_H;
+   }
+}
