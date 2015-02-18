241054b06ae30d5a3f15ea1fd6f75eec588be216
knovakovskaya <k9119097207@gmail.com>
2015-02-18 15:30:04 +0300

    Refactoring. Firts choosin start points algos.
    Common functions and classes moved from KMeans to new packages.
    Algorithms to choose start points added.

--- src/main/java/ru/ifmo/thesis/algo/KMeans.java
+++ src/main/java/ru/ifmo/thesis/algo/KMeans.java
@@ -1,24 +1,14 @@
 package ru.ifmo.thesis.algo;
 
-
-import javax.imageio.ImageIO;
-import java.awt.*;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.util.*;
+import java.util.Map.Entry;
 
 public class KMeans {
-    public static final int MODE_CONTINUOUS = 1;
-    public static final int MODE_ITERATIVE = 2;
-    private final static Map<Color, Integer> colors = new HashMap<>();
-    BufferedImage original;
-    BufferedImage result;
-    Cluster[] clusters;
 
-
-    public KMeans() {
-    }
-
+    //TODO: move this functionality from testing method to GUI
+    /* start point added for KMEANS testing */
     public static void main(String[] args) {
         args = new String[] {"/Users/knovakovskaya/Documents/thesis/pics/sample1.jpg", "/Users/knovakovskaya/clusterized.png", "5", "-c"};
 
@@ -35,84 +25,168 @@ public class KMeans {
         String dst = args[1];
         int k = Integer.parseInt(args[2]);
         String m = args[3];
-        int mode = 1;
+        KMeansMode mode = KMeansMode.CONTINUOUS;
         if (m.equals("-i")) {
-            mode = MODE_ITERATIVE;
+            mode = KMeansMode.ITERATIVE;
         } else if (m.equals("-c")) {
-            mode = MODE_CONTINUOUS;
+            mode = KMeansMode.CONTINUOUS;
         }
 
         // create new KMeans object
-        KMeans kmeans = new KMeans();
+        KMeans kmeans = new KMeans(mode, 0.05);
         // call the function to actually start the clustering
-        BufferedImage dstImage = kmeans.calculate(loadImage(src), k, mode);
+        BufferedImage dstImage = kmeans.calculate(PicUtil.loadImage(src), MAX_CLUSTERS_NUM);
         // save the resulting image
-        saveImage(dst, dstImage);
+        PicUtil.saveImage(dst, dstImage);
     }
 
-    public static Collection<Map.Entry<Color, Integer>> calculateAndGetColors(final String filename, int clusterNumber) {
-        final int mode = MODE_CONTINUOUS;
-        assert clusterNumber > 0 && clusterNumber <= 255;
-        KMeans kMeans = new KMeans();
-        colors.clear();
-        kMeans.calculate(loadImage(filename), clusterNumber, mode);
-        return kMeans.returnTopColors();
+    /*
+     * Separator for test functions and KMeans functionality
+     *
+     *
+     */
+
+    public enum KMeansMode {
+        CONTINUOUS, ITERATIVE
     }
 
-    public static void saveImage(String filename,
-                                 BufferedImage image) {
-        File file = new File(filename);
-        try {
-            ImageIO.write(image, "png", file);
-        } catch (Exception e) {
-            System.out.println(e.toString() + " Image '" + filename
-                    + "' saving failed.");
-        }
+    private final static HashMap<Integer, Integer> colors = new HashMap<>();
+    private static final double GLOBAL_PART = 0.05;
+    private static final int MAX_CLUSTERS_NUM = 25;
+    private static final int TOP_COLORS = 500;
+    private static final int TOO_MANY_COLORS = 5000;
+    private KMeansMode mode;
+    private double border;
+    private Cluster[] clusters;
+
+
+    public KMeans() {
+        mode = KMeansMode.CONTINUOUS;
+        border = -1.0;
     }
 
-    public static BufferedImage loadImage(String filename) {
-        BufferedImage result = null;
-        try {
-            result = ImageIO.read(new File(filename));
-        } catch (Exception e) {
-            System.out.println(e.toString() + " Image '"
-                    + filename + "' not found.");
+    public KMeans(KMeansMode amode, double aborder){
+        mode = amode;
+        border = aborder;
+    }
+
+    /*
+     * KMeans algo functions. All functions to creates start clusters
+     * and to calculate final clusters.
+     */
+
+    public Cluster[] getStartClustersSimple(BufferedImage image, int k) {
+        Cluster[] result = new Cluster[k];
+        int dx = image.getWidth() / k;
+        int dy = image.getHeight() / k;
+        for (int i = 0, x = 0, y = 0; i < k; i++) {
+            result[i] = new Cluster(i, image.getRGB(x, y));
+            x += dx;
+            y += dy;
         }
         return result;
     }
 
-    public BufferedImage calculate(BufferedImage image,
-                                   int k, int mode) {
+    private Cluster[] randomKClustersFromCollection(Collection<Entry<Integer, Integer>> colors, int k) {
+        Cluster[] clusters = new Cluster[k];
+        ArrayList<Entry<Integer,Integer>> colorsList = new ArrayList<>(colors);
+        Collections.shuffle(colorsList);
+        return clustersFromColorCollection(colorsList.subList(0,k));
+    }
+
+    private Cluster[] clustersFromColorCollection(Collection<Entry<Integer, Integer>> colors){
+        Cluster[] clusters = new Cluster[colors.size()];
+        int id = 0;
+        for (Map.Entry<Integer,Integer> entry: colors){
+            clusters[id] = new Cluster(id, entry.getKey());
+            clusters[id++].pixelCount = entry.getValue();
+        }
+        return clusters;
+    }
+
+    private int getMainClusters(ArrayList<Entry<Integer, Integer>> sortedColors, int k){
+        /* returns number of global(>sum/GLOBAL_PART pixels) colors, but less than k*/
+        int sum = 0, counter = 0;
+        for (Entry<Integer,Integer> entry: sortedColors)
+            sum += entry.getValue();
+        for (Entry<Integer,Integer> entry: sortedColors){
+            if ((counter == k) || ((double)entry.getValue()/sum < GLOBAL_PART))
+                break;
+            sum -= entry.getValue();
+            counter++;
+        }
+        return counter;
+    }
+
+    private ArrayList<Entry<Integer, Integer>> getDiagColors(ArrayList<Entry<Integer, Integer>> sortedColors, int k){
+        int step = sortedColors.size()/k;
+
+        ArrayList<Entry<Integer, Integer>> result = new ArrayList<>();
+        for (int i = 0; i < k; ++i)
+            result.add(sortedColors.get(i*step));
+
+        return result;
+    }
+
+    public Cluster[] getSmartKClusters(BufferedImage image, int k) {
+        HashMap<Integer,Integer> colors = PicUtil.parseColorsFromPicture(image);
+        if (colors.size() > k) {
+            //remove with border
+            int size = image.getHeight() * image.getWidth();
+            LinkedList<Integer> toRemove = new LinkedList<>();
+            for (Entry<Integer,Integer> entry: colors.entrySet()){
+                if ( (border > 0) && (colors.size() > k) && (((double)entry.getValue())/size < border) )
+                    toRemove.push(entry.getKey());
+            }
+            for (Integer key: toRemove)
+                colors.remove(key);
+
+            if (colors.size() > TOO_MANY_COLORS){
+                //if still a lot of (>5k colors) let get random colors
+                return randomKClustersFromCollection(new ArrayList<Entry<Integer, Integer>>(colors.entrySet()), k);
+            }else {
+                ArrayList<Entry<Integer, Integer>> sortedColors = new ArrayList<>(PicUtil.getSortedColors(colors));
+                int mainClustersNum = getMainClusters(sortedColors, k);
+                ArrayList<Entry<Integer,Integer>> topColors = new ArrayList<>(sortedColors.subList(0, mainClustersNum));
+                if (mainClustersNum < k)
+                    topColors.addAll(
+                            getDiagColors(
+                                    new ArrayList<Entry<Integer,Integer>>(
+                                            sortedColors.subList(mainClustersNum, sortedColors.size())
+                                    ),
+                                    k
+                            )
+                    );
+                return clustersFromColorCollection(topColors);
+            }
+        }
+        return clustersFromColorCollection(new ArrayList<Entry<Integer, Integer>>(colors.entrySet()));
+    }
+
+    public BufferedImage calculate(BufferedImage image, int k) {
         long start = System.currentTimeMillis();
         int w = image.getWidth();
         int h = image.getHeight();
-        // create clusters
-        clusters = createClusters(image, k);
+        clusters = getSmartKClusters(image, k);
+        long endfirst = System.currentTimeMillis();
+        System.out.println("Prepared start clusters in " + (endfirst - start) + " ms.");
         // create cluster lookup table
         int[] lut = new int[w * h];
         Arrays.fill(lut, -1);
 
-        // at first loop all pixels will move their clusters
         boolean pixelChangedCluster = true;
-        // loop until all clusters are stable!
         int loops = 0;
-        while (pixelChangedCluster) {
+        while (pixelChangedCluster && loops <50) {
             pixelChangedCluster = false;
             loops++;
             for (int y = 0; y < h; y++) {
                 for (int x = 0; x < w; x++) {
                     int pixel = image.getRGB(x, y);
-                    Cluster cluster = findMinimalCluster(pixel);
+                    Cluster cluster = findClosestCluster(pixel);
                     if (lut[w * y + x] != cluster.getId()) {
-                        // cluster changed
-                        if (mode == MODE_CONTINUOUS) {
-                            if (lut[w * y + x] != -1) {
-                                // remove from possible previous
-                                // cluster
-                                clusters[lut[w * y + x]].removePixel(
-                                        pixel);
-                            }
-                            // add pixel to cluster
+                        if (mode == KMeansMode.CONTINUOUS) {
+                            if (lut[w * y + x] != -1)
+                                clusters[lut[w * y + x]].removePixel( pixel);
                             cluster.addPixel(pixel);
                         }
                         // continue looping
@@ -123,17 +197,14 @@ public class KMeans {
                     }
                 }
             }
-            if (mode == MODE_ITERATIVE) {
+            if (mode == KMeansMode.ITERATIVE) {
                 // update clusters
-                for (int i = 0; i < clusters.length; i++) {
-                    clusters[i].clear();
-                }
+                for (Cluster cluster : clusters)
+                    cluster.clear();
                 for (int y = 0; y < h; y++) {
                     for (int x = 0; x < w; x++) {
                         int clusterId = lut[w * y + x];
-                        // add pixels to cluster
-                        clusters[clusterId].addPixel(
-                                image.getRGB(x, y));
+                        clusters[clusterId].addPixel(image.getRGB(x, y));
                     }
                 }
             }
@@ -146,7 +217,7 @@ public class KMeans {
             for (int x = 0; x < w; x++) {
                 int clusterId = lut[w * y + x];
                 result.setRGB(x, y, clusters[clusterId].getRGB());
-                final Color currentColor = new Color(clusters[clusterId].getRGB());
+                int currentColor = clusters[clusterId].getRGB();
                 if (colors.containsKey(currentColor)) {
                     colors.put(currentColor, colors.get(currentColor) + 1);
                 } else {
@@ -161,125 +232,39 @@ public class KMeans {
         return result;
     }
 
-    private Collection<Map.Entry<Color, Integer>> returnTopColors() {
-        ArrayList<Map.Entry<Color, Integer>> colorList = new ArrayList<>(colors.entrySet());
-        Collections.sort(colorList, new Comparator<Map.Entry<Color, Integer>>() {
-            @Override
-            public int compare(final Map.Entry<Color, Integer> o1, final Map.Entry<Color, Integer> o2) {
-                return -o1.getValue() + o2.getValue();
-            }
-        });
-        return colorList;
-    }
 
-    public Cluster[] createClusters(BufferedImage image, int k) {
-        // Here the clusters are taken with specific steps,
-        // so the result looks always same with same image.
-        // You can randomize the cluster centers, if you like.
-        Cluster[] result = new Cluster[k];
-        int x = 0;
-        int y = 0;
-        int dx = image.getWidth() / k;
-        int dy = image.getHeight() / k;
-        for (int i = 0; i < k; i++) {
-            result[i] = new Cluster(i, image.getRGB(x, y));
-            x += dx;
-            y += dy;
-        }
-        return result;
-    }
+    /*
+     * some functions, that helps to calculate something for KMeans
+     */
 
-    public Cluster findMinimalCluster(int rgb) {
-        Cluster cluster = null;
+    public Cluster findClosestCluster(int rgb) {
+        Cluster closest_cluster = null;
         int min = Integer.MAX_VALUE;
-        for (int i = 0; i < clusters.length; i++) {
-            int distance = clusters[i].distance(rgb);
+        for (Cluster cluster : clusters){
+            int distance = cluster.distance(rgb);
             if (distance < min) {
                 min = distance;
-                cluster = clusters[i];
+                closest_cluster = cluster;
             }
         }
-        return cluster;
+        return closest_cluster;
     }
 
-    class Cluster {
-        int id;
-        int pixelCount;
-        int red;
-        int green;
-        int blue;
-        int reds;
-        int greens;
-        int blues;
 
-        public Cluster(int id, int rgb) {
-            int r = rgb >> 16 & 0x000000FF;
-            int g = rgb >> 8 & 0x000000FF;
-            int b = rgb >> 0 & 0x000000FF;
-            red = r;
-            green = g;
-            blue = b;
-            this.id = id;
-            addPixel(rgb);
-        }
-
-        public void clear() {
-            red = 0;
-            green = 0;
-            blue = 0;
-            reds = 0;
-            greens = 0;
-            blues = 0;
-            pixelCount = 0;
-        }
-
-        int getId() {
-            return id;
-        }
-
-        int getRGB() {
-            int r = reds / pixelCount;
-            int g = greens / pixelCount;
-            int b = blues / pixelCount;
-            return 0xff000000 | r << 16 | g << 8 | b;
-        }
-
-        void addPixel(int color) {
-            int r = color >> 16 & 0x000000FF;
-            int g = color >> 8 & 0x000000FF;
-            int b = color >> 0 & 0x000000FF;
-            reds += r;
-            greens += g;
-            blues += b;
-            pixelCount++;
-            red = reds / pixelCount;
-            green = greens / pixelCount;
-            blue = blues / pixelCount;
-        }
-
-        void removePixel(int color) {
-            int r = color >> 16 & 0x000000FF;
-            int g = color >> 8 & 0x000000FF;
-            int b = color >> 0 & 0x000000FF;
-            reds -= r;
-            greens -= g;
-            blues -= b;
-            pixelCount--;
-            red = reds / pixelCount;
-            green = greens / pixelCount;
-            blue = blues / pixelCount;
-        }
-
-        int distance(int color) {
-            int r = color >> 16 & 0x000000FF;
-            int g = color >> 8 & 0x000000FF;
-            int b = color >> 0 & 0x000000FF;
-            int rx = Math.abs(red - r);
-            int gx = Math.abs(green - g);
-            int bx = Math.abs(blue - b);
-            int d = (rx + gx + bx) / 3;
-            return d;
-        }
+    /*
+     * static methods that returns KMeans result
+     * TODO: create KMeansResult class and return its object as a result
+     */
+    public static Collection<Map.Entry<Color, Integer>> calculateAndGetColors(final String filename,
+                                                                              int clusterNumber,
+                                                                              KMeansMode mode,
+                                                                              double border) {
+        assert clusterNumber > 0 && clusterNumber <= MAX_CLUSTERS_NUM;
+        KMeans kMeans = new KMeans(mode, border);
+        colors.clear();
+        kMeans.calculate(PicUtil.loadImage(filename), clusterNumber);
+        return PicUtil.castToColorArray(PicUtil.getSortedColors(kMeans.colors));
     }
+
 
 }
